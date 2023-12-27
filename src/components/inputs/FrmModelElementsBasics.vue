1416522d9c2bb192491b5de37c50bd21eea6740d
Jose Javier Velarde <51091403+Jos-JV@users.noreply.github.com>
2023-12-27 17:52:27 -0400

    se mejoro la interfaz y el combo box

--- src/components/inputs/FrmModelElementsBasics.vue
+++ src/components/inputs/FrmModelElementsBasics.vue
@@ -1,10 +1,18 @@
 <template>
   <v-row v-if="!Array.isArray(item.valores)">
-    <v-col cols="12" xs="12" sm="12" md="6" :xl="lengthCols" :lg="lengthCols"
-      v-for="(obj, nameObj) in item.campos" :key="'srhTb' + nameObj" >
-      
+    <v-col class="cajabasico"
+      cols="12"
+      xs="12"
+      sm="12"
+      md="6"
+      :xl="lengthCols"
+      :lg="lengthCols"
+      v-for="(obj, nameObj) in item.campos"
+      :key="'srhTb' + nameObj"
+    >
+    <div>{{ obj[3] }}</div>
       <!-- solo texto -->
-      <v-text-field
+      <v-text-field class="nuevo"
         v-model="item.valores[nameObj]"
         type="text"
         :label="obj[0]"
@@ -12,11 +20,13 @@
         :filled="!obj[1]"
         :rounded="!obj[1]"
         :maxlength="obj[4]"
-        dense
-        v-if ="obj[3] == 'TT' && !obj[5]"
+        outlined
+        clearable
+        
+        v-if="obj[3] == 'TT' && !obj[5]"
       />
       <!-- solo numero -->
-      <v-text-field
+      <v-text-field class="nuevo"
         v-model="item.valores[nameObj]"
         type="number"
         :label="obj[0]"
@@ -24,11 +34,13 @@
         :filled="!obj[1]"
         :rounded="!obj[1]"
         :maxlength="obj[4]"
-        dense
-        v-else-if ="obj[3] == 'TN' && !obj[5]"
+        outlined
+        clearable
+        
+        v-else-if="obj[3] == 'TN' && !obj[5]"
       />
       <!-- solo email -->
-      <v-text-field
+      <v-text-field class="nuevo"
         v-model="item.valores[nameObj]"
         type="email"
         :label="obj[0]"
@@ -37,20 +49,25 @@
         :filled="!obj[1]"
         :rounded="!obj[1]"
         :maxlength="obj[4]"
-        dense
-        v-else-if ="obj[3] == 'TM' && !obj[5]"
+        outlined
+        clearable
+        
+        v-else-if="obj[3] == 'TM' && !obj[5]"
       />
       <!-- solo decimal -->
-      <v-text-field
+      <v-text-field class="nuevo mejorando"
         v-model="item.valores[nameObj]"
         type="number"
-        step="0.000000000001" max="999.999"
+        step="0.000000000001"
+        max="999.999"
         :label="obj[0]"
         :readonly="!obj[1]"
         :filled="!obj[1]"
         :rounded="!obj[1]"
-        dense
-        v-else-if ="obj[3] == 'TD' && !obj[5]"
+        outlined
+        clearable
+        
+        v-else-if="obj[3] == 'TD' && !obj[5]"
       />
 
       <!-- texto abierto -->
@@ -61,13 +78,16 @@
         :label="obj[0]"
         :maxlength="obj[4]"
         rows="1"
-        v-else-if ="obj[3] == 'TA' && !obj[5]"
+        v-else-if="obj[3] == 'TA' && !obj[5]"
       />
 
-      
       <combo-box-forms
         v-model="item.valores[nameObj].selected"
-        :items-combo="item.valores[nameObj].items[item.valores[item.valores[nameObj].dependency].selected.value]"
+        :items-combo="
+          item.valores[nameObj].items[
+            item.valores[item.valores[nameObj].dependency].selected.value
+          ]
+        "
         :label="obj[0]"
         :readonly="!obj[1]"
         :filled="!obj[1]"
@@ -79,14 +99,17 @@
         v-else-if="
           obj[3] == 'C' &&
           item.valores[nameObj] &&
-          item.valores[nameObj].selected && item.valores[nameObj].dependency && 
-          item.valores[nameObj].items[item.valores[item.valores[nameObj].dependency].selected.value] &&
+          item.valores[nameObj].selected &&
+          item.valores[nameObj].dependency &&
+          item.valores[nameObj].items[
+            item.valores[item.valores[nameObj].dependency].selected.value
+          ] &&
           !obj[5]
         "
       />
 
-    
       <combo-box-forms
+       
         v-model="item.valores[nameObj].selected"
         :items-combo="item.valores[nameObj].items"
         :label="obj[0]"
@@ -104,18 +127,77 @@
           !obj[5]
         "
       />
+      
+      <v-radio-group class="boxnuevo mt-n2 "
+        row 
+        no-gutters v-else-if="
+          obj[3] == 'R' &&
+          item.valores[nameObj] &&
+          item.valores[nameObj].selected &&
+          !obj[5]
+          "
+        v-model="item.valores[nameObj].selected.value" 
+      >
+      <template v-slot:label>
+        <div>{{obj[0]}}</div>
+      </template> 
+      <v-radio
+          v-for="(valor) in item.valores[nameObj].items"
+          :key="valor.value"
+          :label="valor.text"
+          :value="valor.value"
+        ></v-radio>
+      </v-radio-group>
+
+      <!-- <check-forms v-else-if="
+            obj[3] == 'H' &&
+            item.valores[nameObj] &&
+            item.valores[nameObj].selected &&
+            !obj[5]"
+        :items="item.valores[nameObj].items"
+        :label="obj[0]"
+        :selected="item.valores[nameObj].selected"
+      ></check-forms> -->
+
+      <v-container v-else-if="
+            obj[3] == 'H' &&
+            item.valores[nameObj] &&
+            item.valores[nameObj].selected &&
+            !obj[5]">
+        <v-container>{{obj[0]}}</v-container>
+          <div> 
+          </div>S
+            <v-checkbox
+              v-for="obj in item.valores[nameObj].items" :key="obj.value"
+              v-model="item.valores[nameObj].selected.value"
+              :label="obj.text"
+              :value="obj.value"
+              :readonly="obj[1]"
+              hide-details
+            />
+      </v-container>
+
+      <v-switch
+        v-model="item.valores[nameObj].selected"
+        hide-details
+        color="blue"
+        true-value="Si"
+        false-value="No"
+        :model-value="true"
+        :disabled="obj[1]"
+        :label="item.valores[nameObj].selected"
+        v-else-if="obj[3] == 'SW' && !obj[5]"/>
+
       <CalendarForms
         v-model="item.valores[nameObj]"
         :label="obj[0]"
         :name="nameObj"
         v-else-if="obj[3] == 'F' && !obj[5]"
       />
-      
+
       <div v-else>
         <div class="font-weight-black">{{ obj[0] }}:</div>
-        <span v-if="obj[3] == 'TT'">
-          {{ item.valores[nameObj] }} </span
-        >
+        <span v-if="obj[3] == 'TT'"> {{ item.valores[nameObj] }} </span>
         <span v-else> {{ item.valores[nameObj]?.selected?.text }}</span>
       </div>
     </v-col>
@@ -139,6 +221,7 @@
 <script>
 import ComboBoxForms from "@/components/inputs/ComboBoxForms.vue";
 import CalendarForms from "@/components/inputs/CalendarForms.vue";
+import CheckForms from "./CheckForms.vue";
 
 import ContenedorAlert from "@/components/utils/ContenedorAlert.vue";
 
@@ -147,21 +230,24 @@ export default {
     ComboBoxForms,
     CalendarForms,
     ContenedorAlert,
+    CheckForms,
   },
   name: "FrmByModelElementsBasics",
   props: {
     value: { type: Object, default: () => ({}) },
     lengthCols: { type: Number, default: 3 },
   },
-  data () {
-      return {
-        rulesEmail: {
-          email: value => {
-            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            return pattern.test(value) || 'Invalid e-mail.'
-          },
+  data() {
+    return {
+      datos:[],
+      rulesEmail: {
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
         },
-      }
+      },
+    };
   },
 
   computed: {
@@ -179,8 +265,33 @@ export default {
       if (val === null) {
         return false;
       }
-      return  typeof val === "object";
+      return typeof val === "object";
     },
-  },
+    
+  }
 };
 </script>
+<style>
+.cajabasico{
+  
+  height: 85px;
+}
+.nuevo{
+  
+  height: 100px;
+}
+.mejorando{
+  background-color: null;
+  border-radius: 5px;
+  height: 95px;
+}
+.boxnuevo{
+  
+  background-color: rgb(243, 243, 243);
+  height: 70px;
+  padding: 8px;
+  border-radius: 15px;
+ /*  border: 1px solid rgb(0, 0, 0); */
+  
+}
+</style>
